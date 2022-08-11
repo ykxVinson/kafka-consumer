f83a72dbf930022041eba242b4b53f385587cbfb
Vinson <ykx.vinson@gmail.com>
2022-08-11 18:44:08 +0800

    实现接受数据从kafka

---
+++ src/main/java/com/vinson/component/TopicComponent.java
@@ -0,0 +1,15 @@
+package com.vinson.component;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TopicComponent {
+    @KafkaListener(topics = {"first"})
+    public void onMessage(ConsumerRecord<String,String> record){
+        String topic = record.topic();
+        String msg = record.value();
+        System.out.println("消费者接收消息： topic--> " + topic + ", msg --> "+ msg);
+    }
+}
